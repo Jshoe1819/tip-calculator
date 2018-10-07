80bf9757a08e3b306b94b1d327267bb2906f1724
Jacob Shoemaker <Jshoe1819@gmail.com>
2018-10-06 22:00:43 -0400

    first commit

--- tip-calculator/ViewController.swift
+++ tip-calculator/ViewController.swift
@@ -6,6 +6,19 @@
 //  Copyright © 2018 Jacob Shoemaker. All rights reserved.
 //
 
+//bottom height update
+//allow round to if round split not none
+//format of bill total input
+//format of tip input
+//change tip % if tip input
+//change tip % when slider change
+//change tip input when slider change
+//change split when split slider change
+//adjust total cost
+//adjust split cost
+//adjust split cost if round changed
+//adjust split cost if round to change
+
 import UIKit
 
 class ViewController: UIViewController {
